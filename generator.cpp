fca11a4e8c90abac015aac8ea80c38d17c5bfb50
Makar Baderko <makarbaderko@gmail.com>
2022-10-30 14:05:14 +0300

    Code review

--- generator.cpp
+++ generator.cpp
@@ -6,18 +6,18 @@
 using namespace std;
 
 int main(){
-    int inside = 0;
-    double total = 100000000;
-    float x, y, vec_len;
+    int inside = 0; // number of points inside the circle
+    double total = 100000000; // total number of points to be generated. More points -> better results
+    float x, y, vec_len; 
     for (int i = 0; i < total; i++){
-        x = (float)rand() / (float)RAND_MAX;
+        x = (float)rand() / (float)RAND_MAX; //generating point's coordinates [0, 1]
         y = (float)rand() / (float)RAND_MAX;
-        vec_len = pow(x,2) + pow(y,2);
-        if (vec_len <= 1){
+        vec_len = pow(x,2) + pow(y,2); // calculate the distance from [0, 0] to [x, y]
+        if (vec_len <= 1){ //determine whether the point is inside th circle (circle radius is 1)
             inside++;
         }
     }
-    double ans = (double)(inside/total)*4.0;
-    cout << fixed << setprecision(10) << ans << endl;
+    double pi = (double)(inside/total)*4.0;  // calculate pi using areas' formulae
+    cout << fixed << setprecision(10) << pi << endl;
     return 0;
 }
